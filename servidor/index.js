13d83ae3e827c1e3f5e3103d9be5d4b6bb8b0a4b
Emmanuel <115524511+Trexpapu@users.noreply.github.com>
2023-11-29 23:00:33 -0600

    bugs arreglados con cliente-cliente

--- servidor/index.js
+++ servidor/index.js
@@ -17,6 +17,7 @@ const io = new Server(server, {
 
 const Lista_sockets = new Map();//lista donde se guarda id y objeto socket
 const solicitudesPendientes = new Map();
+const lista_sockets_en_chat = new Map();
 //Conexion del web socket usuarios
 io.on('connection', (socket) => {//permite escuchar las connexiones de los clientes, recuperamos el socket
     console.log('\nUn usuario se a conectado', socket.id)
@@ -49,9 +50,9 @@ io.on('connection', (socket) => {//permite escuchar las connexiones de los clien
         io.emit('updateUsersList', userList);
     }
 
-    socket.on('Individual_m',(msg, otro_id) =>{ //cuando un cliente solicita un mensaje individual se recibe el mensaje y el id al cliente receptor
+    socket.on('Individual_m',(msg, otro_id, salida) =>{ //cuando un cliente solicita un mensaje individual se recibe el mensaje y el id al cliente receptor
         const socketDestino = Lista_sockets.get(otro_id);//buscamos el id en la lista para obtener el socket asociado
-        if (socketDestino){
+        if (socketDestino && salida == false){
             socket.emit('Mensaje_individual', msg)//Llamamos el evento mensaje individual para el emisor
             socketDestino.emit('Mensaje_individual', msg)//igualmente para el receptor
         }else{
@@ -66,29 +67,53 @@ io.on('connection', (socket) => {//permite escuchar las connexiones de los clien
         const socketDestino = Lista_sockets.get(id_usuario);
         if (socket.id !== id_usuario) {
             // Verificar si ya hay una solicitud pendiente para este usuario
-            if (!solicitudesPendientes.has(id_usuario)) {
-                // Agregar la solicitud a la lista de solicitudes pendientes
-                solicitudesPendientes.set(id_usuario, socket.id);
 
                 // Enviar la solicitud al usuario de destino
                 socketDestino.emit('Solicitud de chat', {
                     remitente: socket.id,
                     
                 });
-            }
+                socket.emit('En espera de solicitud de chat', true)
+            
+        }
+    })
+
+    socket.on('Liberar solicitud', (id)=>{
+        const socketDestino = Lista_sockets.get(id);
+        if (socketDestino){
+            socketDestino.emit('En espera de solicitud de chat', false)
         }
     })
 
 
     socket.on('Aceptar chat individual', (id_remitente) => {//Si el cliente acepta la solicitud manda el evento redirec2 al cliente solicitante y al que acepta
         const socketRemitente = Lista_sockets.get(id_remitente);
-        if (socket.id !== id_remitente) {
+        const socket_en_chat = lista_sockets_en_chat.get(socket.id)
+        if  (socket.id !== id_remitente) {
+            if (socket_en_chat){
+                socket.emit('Avisar_que_salio')
+                console.log("xd")
+            }
+            lista_sockets_en_chat.set(id_remitente, socketRemitente);
+            lista_sockets_en_chat.set(socket.id, socket);
             socket.emit('redirect2', id_remitente)
             socketRemitente.emit('redirect2', socket.id)
-            
+
+            console.log(lista_sockets_en_chat)
         }
 
     });
+
+    socket.on('Sali', (id) => {
+        const socketDestino = Lista_sockets.get(id);
+    
+        if (socketDestino) { // Verificar si socketDestino existe
+            socketDestino.emit('Quedaste_solo');
+        } else {
+            console.log(`Error: No se encontró el socket para el usuario con ID ${id}`);
+        }
+    });
+    
 
 })
 
