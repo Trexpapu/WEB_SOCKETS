b989827e1ea00a4249d3c80fcf5971d497547b3d
Scarlett <1577852e@umich.mx>
2023-12-04 00:50:57 -0600

    Cambios en la interfaz y agregar ID al mandar mensaje

--- servidor/index.js
+++ servidor/index.js
@@ -31,7 +31,7 @@ io.on('connection', (socket) => {//permite escuchar las connexiones de los clien
     })
 
     socket.on('chat message', async (msg) =>{ //El servidor recibe una peticion chat message
-        io.emit('chat message', msg) //El servidor emite un broadcast con el mensaje que recibe en la funcion
+        io.emit('chat message', msg, socket.id) //El servidor emite un broadcast con el mensaje que recibe en la funcion
     })
      // Cuando un cliente hace la peticion Chat grupal emitimos redirect que redirecciona a otro html
      socket.on('Chat grupal', () => {
